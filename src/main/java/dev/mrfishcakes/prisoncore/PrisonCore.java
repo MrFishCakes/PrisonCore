7e335a5ec56789ec60f7d96a56e7263085cb7348
FallenTM <kgonzo47@gmail.com>
2021-10-29 18:29:56 -0400

    Added BlockBreak Event, updated PrisonCore

--- src/main/java/dev/mrfishcakes/prisoncore/PrisonCore.java
+++ src/main/java/dev/mrfishcakes/prisoncore/PrisonCore.java
@@ -3,9 +3,11 @@ package dev.mrfishcakes.prisoncore;
 import dev.mrfishcakes.crunchy.enchantment.CustomEnchantment;
 import dev.mrfishcakes.prisoncore.enchantments.EnchantmentListener;
 import dev.mrfishcakes.prisoncore.enchantments.combat.BeheadingEnchantment;
+import dev.mrfishcakes.prisoncore.event.BlockBreak;
 import dev.mrfishcakes.prisoncore.registry.Registry;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,6 +21,11 @@ public final class PrisonCore extends JavaPlugin {
     @Override
     public void onEnable() {
         registerEnchantments();
+    }
+
+    private void registerEvents() {
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new BlockBreak(), this);
     }
 
     private void registerEnchantments() {
