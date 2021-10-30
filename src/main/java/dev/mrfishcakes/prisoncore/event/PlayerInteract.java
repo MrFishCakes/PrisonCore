800fff7d16124aad428cd037d1df8d222e8fc64b
FallenTM <kgonzo47@gmail.com>
2021-10-29 23:40:30 -0400

    Fixed Crate Errors with Interact Event.

--- src/main/java/dev/mrfishcakes/prisoncore/event/PlayerInteract.java
+++ src/main/java/dev/mrfishcakes/prisoncore/event/PlayerInteract.java
@@ -1,8 +1,8 @@
 package dev.mrfishcakes.prisoncore.event;
 
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerInteract implements Listener {
@@ -11,10 +11,19 @@ public class PlayerInteract implements Listener {
 
     @EventHandler
     public void on(PlayerInteractEvent e) {
-        if (e.getClickedBlock().hasMetadata("Vote")) {
-            if (e.getClickedBlock().getType().equals(Material.CHEST)) {
-                e.setCancelled(true);
-                e.getPlayer().sendMessage("You clicked Vote crate.");
+        if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.LEFT_CLICK_AIR)) {
+            return;
+        } else if (e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+            return;
+        }
+        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) ) {
+            if (e.getClickedBlock().hasMetadata("Vote")) {
+                if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                    e.setCancelled(true);
+                    e.getPlayer().sendMessage("You clicked Vote crate.");
+                }
+            } else {
+                return;
             }
         }
     }
